98d4c6856da639b072d5d9e43cbb6fe44b263d0d
Dc0rLEonE <nikkiecabs548@gmail.com>
2021-10-20 17:24:14 +0800

    Update

---
+++ DisemvowelTrolls/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DisemvowelTrolls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Disemvowel("Hello World!"));
+            Console.ReadKey();
+        }
+
+        private static string Disemvowel(string str)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            List<char> listOfVowels = vowels.OfType<char>().ToList();
+
+            StringBuilder sb = new StringBuilder(str);
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (listOfVowels.Contains(sb[i]))
+                {
+                    sb.Replace(sb[i].ToString(), "");
+                    i--;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TwoSum
 {
@@ -6,18 +7,64 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            int[] theNumbers = new int[3] {3,2,4 };
-            Console.WriteLine(TwoSum(theNumbers,6));
+            int[] theNumbers = new int[2] { 3, 2 };
+            Console.WriteLine(TwoSum(theNumbers, 5));
             Console.ReadKey();
         }
 
-        private static int [] TwoSum (int [] numbers, int target)
+        private static int[] TwoSum(int[] numbers, int target)
         {
+            //Solution 1
+            //foreach (var number in numbers)
+            //{
+            //    var num = target - number;
+            //    if (numbers.Contains(num))
+            //    {
+            //        var index1 = Array.IndexOf(numbers, num);
+            //        var index2 = Array.LastIndexOf(numbers, number);
+            //        return new int[] { index1, index2 };
+            //    }
+            //}
+            //return new int[0];
+
+            //Solution 2
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    for (int j = 0; j < numbers.Length; j++)
+            //    {
+            //        if (numbers[i] + numbers[j] == target & i != j)
+            //            return new int[] { i, j };
+            //    }
+            //}
+
+            //return new int[] { };
+
+            //Solution 3
             for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine(numbers[i]);
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == target)
+                        return new int[] { i, j };
+                }
             }
-            return numbers;
+            return new int[] { };
+
+            //Test
+            //int[] numbers = new int[4] { 1, 2, 3, 4 };
+            //var target = 5;
+            //int sum = 0, result = 0;
+
+
+            //Random random = new Random();
+            //int length = random.Next(numbers.Length - 1);
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum = numbers[i] + numbers[length];
+            //    Console.WriteLine(Array.IndexOf(numbers, i));
+            //}
+
         }
     }
 }
